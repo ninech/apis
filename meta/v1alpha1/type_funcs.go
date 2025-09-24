bfc56eb663b17882a3d2d18391943fc47c7d3e11
Pawel Kuc <pawel@nine.ch>
2025-09-24 15:37:16 +0200

    feat: expose DNS verification helpers for custom hostname status

--- meta/v1alpha1/type_funcs.go
+++ meta/v1alpha1/type_funcs.go
@@ -7,3 +7,25 @@ import (
 func (t TypedReference) String() string {
 	return fmt.Sprintf("%s/%s (%s)", t.Namespace, t.Name, t.GroupKind.String())
 }
+
+// String implements the stringer interface.
+func (d DNSCheckType) String() string {
+	return string(d)
+}
+
+// CheckTypeEntry returns the corresponding checkType entry for the given host. The
+// second return parameter indicates if an entry was found. There should always
+// just be one entry per checkType in a DNSVerificationStatusEntries.
+func (vsl DNSVerificationStatusEntries) CheckTypeEntry(host string, checkType DNSCheckType) (DNSVerificationStatusEntry, bool) {
+	for _, s := range vsl {
+		if s.Name == host && s.CheckType == checkType {
+			return s, true
+		}
+	}
+	return DNSVerificationStatusEntry{}, false
+}
+
+// Verified returns if the host of the VerificationStatus was successfully verified.
+func (vs DNSVerificationStatusEntry) Verified() bool {
+	return (vs.CheckType == DNSCheckTXT || vs.CheckType == DNSCheckCNAME) && vs.LatestSuccess != nil
+}
