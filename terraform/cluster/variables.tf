b344401c396a109c69c68355b09c4b71e19f894c
Davor Gajic <davor@nine.ch>
2025-10-21 15:02:21 +0200

    add tf examples link

--- terraform/cluster/variables.tf
+++
@@ -1,9 +0,0 @@
-variable "name" {
-  default     = "tf-sample"
-  description = "name of the cluster and associated resources"
-}
-
-variable "namespace" {
-  default     = "<your cockpit account name>"
-  description = "namespace to create the resources in (matches cockpit account name)"
-}
